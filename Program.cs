7d9239c2be1555cae1f4cd7db800ca183f2b319e
Andre Luige <andreluige@hot.ee>
2022-09-15 15:21:24 +0300

    testing ref and out

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace RefVSOut {
+    class Program {
+        static void Main(string[] args) {
+            int first = 1;
+            int second = 2;
+
+            Console.WriteLine($"first: {first}, second: {second}");
+            Method1(out first, out second);
+            Console.WriteLine($"first: {first}, second: {second}");
+            Method2(first, ref second);
+            Console.WriteLine($"first: {first}, second: {second}");
+            Method3(ref first, second);
+
+            Console.WriteLine($"first: {first}, second: {second}");
+            Console.ReadLine();
+        }
+
+        static void Method1(out int a, out int b) {
+            a = 2;
+            b = 1;
+        }
+
+        static void Method2(int a, ref int b) {
+            a *= 5;
+            b *= a;
+        }
+
+        static void Method3(ref int a, int b) {
+            a *= 5;
+            b *= a;
+        }
+    }
+}
